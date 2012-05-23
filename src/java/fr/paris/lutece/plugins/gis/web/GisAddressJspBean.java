faf09766e9d56a20736861bf67d82f589a3e32cc
KLU <KLU@bab10101-e421-0410-a517-8ce0973de3ef>
2012-05-23 10:11:52 +0000

    GIS23: Plugin GIS must not depend on Module GIS Address, even in Spring context configuration
    git-svn-id: http://dev.lutece.paris.fr/svn/lutece/portal/trunk/plugins/technical/plugin-gis@40561 bab10101-e421-0410-a517-8ce0973de3ef

--- src/java/fr/paris/lutece/plugins/gis/web/GisAddressJspBean.java
+++ src/java/fr/paris/lutece/plugins/gis/web/GisAddressJspBean.java
@@ -5,8 +5,14 @@ import java.rmi.RemoteException;
 
 import javax.servlet.http.HttpServletRequest;
 
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+
 import fr.paris.lutece.plugins.gis.business.LonLat;
 import fr.paris.lutece.plugins.gis.service.IAddressServiceFacade;
+import fr.paris.lutece.portal.service.i18n.I18nService;
 import fr.paris.lutece.portal.service.spring.SpringContextService;
 import fr.paris.lutece.portal.web.admin.PluginAdminPageJspBean;
 
@@ -20,19 +26,33 @@ public class GisAddressJspBean extends PluginAdminPageJspBean
 	 * public static members
 	 */
     public static final String RIGHT_MANAGE_GIS = "GIS_MANAGEMENT";
+    private static final String PROPERTY_ERROR = "gis.plugin.error";
 
 	/**
 	 * private static members
 	 */
-	private static IAddressServiceFacade addressServiceFacade = null;
+	private static IAddressServiceFacade addressServiceFacade = null;	
+	private static Logger logger = null;
 	
-	/**
-	 * Initializes private static members
-	 */
-	static{
-		GisAddressJspBean.addressServiceFacade = (IAddressServiceFacade) 
+	
+	static {
+		logger = Logger.getLogger( GisAddressJspBean.class.getName() );
+	}
+	
+	
+	private static IAddressServiceFacade getAddressFacade() {
+		
+		if ( GisAddressJspBean.addressServiceFacade != null)
+			return addressServiceFacade;
+		
+		try {
+			GisAddressJspBean.addressServiceFacade = (IAddressServiceFacade) 
 				SpringContextService.getBean( "adresseServiceFacade" );
-	};
+		} catch ( NoSuchBeanDefinitionException e ) {
+			logger.log(Level.SEVERE, I18nService.getLocalizedString(PROPERTY_ERROR, Locale.getDefault()), e);
+		}
+		return addressServiceFacade;
+	}
 	
 	/**
 	 * GetGeolocalization( ).
@@ -47,9 +67,9 @@ public class GisAddressJspBean extends PluginAdminPageJspBean
 		// gets HTTP request parameters
 		String address = request.getParameter("address").trim();
 		String srid = request.getParameter("srid").trim();
-		
+		LonLat lonLat = null;
 		// does geolocalization
-		LonLat lonLat =  addressServiceFacade.getGeolocalization(request, address, srid);
+		lonLat =  GisAddressJspBean.getAddressFacade().getGeolocalization(request, address, srid);
 		
 		return lonLat != null ? lonLat.toString() : "";
 	}
@@ -76,7 +96,7 @@ public class GisAddressJspBean extends PluginAdminPageJspBean
 		){
 			LonLat lonLat = new LonLat(Double.parseDouble(x),Double.parseDouble(y));
 			// does inverse geolocalization
-			address = addressServiceFacade.getInverseGeolocalization(request, lonLat, srid); 
+			address = GisAddressJspBean.getAddressFacade().getInverseGeolocalization(request, lonLat, srid); 
 		}
 		
 		return address;
